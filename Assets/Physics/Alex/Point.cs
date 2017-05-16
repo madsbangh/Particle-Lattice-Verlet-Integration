d97897e41d41e9f2ed1b75bc50737d236f03e5c4
Alexander Arendttorp <alexanderarendttorp@gmail.com>
2017-05-16 02:03:53 +0200

    Ermagherd

---
+++ Assets/Physics/Alex/Point.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Alex.Maybe 
+{
+	public class Point 
+	{
+		
+		private float x; 
+		private float y; 
+		private float z;
+		private float px;
+		private float py;
+		private float pz;
+		private float vx = 0f;
+		private float vy = 0f;
+		private float vz = 0f;
+		private bool pinX = false;
+		private bool pinY = false;
+		private bool pinZ = false;
+
+		private float dx;
+		private float dy;
+		private float dz; 
+		private float dist; 
+
+		public Point(float x, float y, float z)
+		{
+			this.x = this.px = x;
+			this.y = this.py = y; 
+			this.z = this.pz = z; 
+		}
+
+		public void Update(float delta)
+		{
+			//if (pinX && pinY && pinZ)
+			//	continue; 
+
+
+		}
+	}
+}
--- Assets/Physics/Alex/VerletCloth.cs
+++ Assets/Physics/Alex/VerletCloth.cs
@@ -6,19 +6,22 @@ namespace Alex
 {
 	public class VerletCloth : MonoBehaviour 
 	{
-		struct Point 
-		{
-			public Vector3 curPos;
-			public Vector3 oldPos; 
-			public bool moveable; 
-		};
-
 		struct Constraints
 		{
 			public int index0;
 			public int index1; 
 			public float restLength; 
-		}
+		};
+
+		struct Point 
+		{
+			public Vector3 curPos;
+			public Vector3 oldPos; 
+			//public Constraints c; 
+			public bool moveable; 
+		};
+
+
 
 		[SerializeField]
 		private Transform[] bones; 
@@ -35,13 +38,25 @@ namespace Alex
 
 		bool run = false; 
 
+
+		// Take two
+		int accuracy = 5; 
+		int clothY = 8;
+		int clothX = 15; 
+		int spacing = 2; 
+		int tearDist = 60; 
+		float friction = 0.99f; 
+		float bounce = 0.5f; 
+
+
+
 		// Use this for initialization
 		IEnumerator Start () 
 		{
 			CreateGrid ();
 			CreateBones ();
 			run = true; 
-			Debug.Log ("I run"); 
+		//	Debug.Log ("I run"); 
 			yield break; 
 		}
 
@@ -91,16 +106,23 @@ namespace Alex
 			{
 				for (int k = 0; k < m_numpoints; k++) 
 				{
-					Constraints c = m_constraints [i]; 
-					Vector3 delta = m_points [c.index1].curPos - m_points [c.index0].curPos;
-					//float deltaLength = Mathf.Sqrt (Vector3.Dot (delta, delta)); 
-					//float diff = (deltaLength - c.restLength) / deltaLength; 
-					delta *= c.restLength*c.restLength/(Vector3.Dot(delta, delta)+c.restLength*c.restLength)-softness; 
-					//x [0] -= delta;// * 0.5f * diff; 
-					//x[1] += delta;// * 0.5f * diff; 
+					m_points [k].curPos = Vector3.Min (Vector3.Max (m_points[k].curPos, Vector3.zero), new Vector3 (1000, 1000, 1000)); 
 
-					m_points [c.index0].curPos -= delta;// * 0.5f * diff; 
-					m_points [c.index1].curPos += delta;// * 0.5f * diff; 
+				//	if (m_points [k].moveable)
+				//	{
+						Constraints c = m_constraints [k]; 
+						//Constraints c = m_points[k].c; 
+						//float deltaLength = Mathf.Sqrt (Vector3.Dot (delta, delta)); 
+						//float diff = (deltaLength - c.restLength) / deltaLength; 
+						//x [0] -= delta;// * 0.5f * diff; 
+						//x[1] += delta;// * 0.5f * diff; 
+
+						Vector3 delta = m_points [c.index1].curPos - m_points [c.index0].curPos;
+						delta *= c.restLength*c.restLength/(Vector3.Dot(delta, delta)+c.restLength*c.restLength)-softness; 
+						m_points [c.index0].curPos -= delta;// * 0.5f * diff; 
+						m_points [c.index1].curPos += delta;// * 0.5f * diff; 
+				//	}
+
 				}	
 				m_points [0].curPos = new Vector3 (0, 10, 0); 
 			}
@@ -119,11 +141,12 @@ namespace Alex
 		void CreateGrid()
 		{
 			List<Transform> tmpList = new List<Transform> (); 
-
-			for (int i = 0; i < 10; i++) {
-				for (int j = 0; j < 10; j++) {
+	//		float startX = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width,0,0)).x / 2 - clothX * spacing / 2; 
+			for (int y = 0; y < clothY; y++) {
+				for (int x = 0; x < clothX; x++) {
+					
 					GameObject tmp = GameObject.CreatePrimitive (PrimitiveType.Cube);
-					tmp.transform.position = new Vector3 (i, j, 0); 
+					tmp.transform.position = new Vector3 (x, y, 0); 
 					tmp.transform.SetParent (this.transform); 
 					tmpList.Add (tmp.transform); 
 				}
@@ -134,42 +157,108 @@ namespace Alex
 
 		void CreateBones()
 		{
+
+			//List<Transform> tmpList = new List<Transform> (); 
+			List<Point> nPoints = new List<Point> (); 
+			//bones = tmpList.ToArray (); 
+			
 			if (bones == null)
 			{
 				bones = GetComponentsInChildren<Transform> (); 
 				Debug.Log ("I found " + bones.Length + " bones."); 
 			} 
-			else 
-			{
-				Debug.Log ("Already done and I found " + bones.Length + " bones."); 
-			}
+
+
+			//else 
+			//{
+				//Debug.Log ("Already done and I found " + bones.Length + " bones."); 
+			//}
 
 			m_numpoints = bones.Length; 
-			m_points = new Point[m_numpoints]; 
 
-			m_numconstraints = (m_numpoints * m_numpoints) - m_numpoints; 
+			float startX = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width,0,0)).x / 2 - clothX * spacing / 2; 
+			for (int y = 0; y < clothY; y++) {
+				for (int x = 0; x < clothX; x++) {
+					Point newPoint = new Point(); 
+					newPoint.curPos = new Vector3(startX + x * spacing, 20 + y * spacing, 0f);
+					newPoint.oldPos = newPoint.curPos; 
+
+					//newPoint.c.index0 = x + clothX * y; 
+
+					if (y == 0)
+						newPoint.moveable = false; 
+					
+				//	if (x != 0)
+				//		newPoint.c.index1 = m_numpoints - 1; 
+
+				//	if (y != 0)
+				//		newPoint.c.index1 = x + (y - 1) * (clothX +1 );
+
+					
+					//GameObject tmp = GameObject.CreatePrimitive (PrimitiveType.Cube);
+					//tmp.transform.position = new Vector3 (x, y, 0); 
+					//tmp.transform.SetParent (this.transform); 
+					//tmpList.Add (tmp.transform); 
+					nPoints.Add(newPoint); 
+				}
+			}
+
+			m_points = nPoints.ToArray (); //new Point[m_numpoints]; 
+/*			for (int i = 0; i < m_numpoints; i++) 
+			{
+				float len = (m_points[m_points[i].c.index1].curPos - m_points[m_points[i].c.index0].curPos).magnitude;
+				m_points[i].c.restLength = len; 
+				//m_constraints[i].restLength = len;
+				
+			}*/
+			
+			m_numconstraints = clothX * clothY * 2 - clothX - clothY; //(m_numpoints * m_numpoints) - m_numpoints; 
 			m_constraints = new Constraints[m_numconstraints]; 
 
+			int i = 0; 
+			for (int y = 0; y < clothY; y++) {
+				for (int x = 0; x < clothX; x++) {
+					if ((x+1) < clothX)
+					{
+						m_constraints [i].index0 = x + y * clothX; 
+						m_constraints [i].index1 = x + 1 + y * clothX; 
+						float len = (m_points[x].curPos - m_points[x + 1].curPos).magnitude;
+						i++;
+					}
+					if ((y+1) < clothY)
+					{
+						m_constraints [i].index0 = x + y * clothX; 
+						m_constraints [i].index1 = x + clothX + y * clothX; 
+						float len = (m_points[x].curPos - m_points[x + clothX].curPos).magnitude;
+						i++;
+					}
+
+					if (i > m_numconstraints)
+						return; 
+				}
+			}
+
+			/*
 			for (int i = 0; i < m_numpoints; i++) {
 				Vector3 pos = bones [i].transform.position; 
 				m_points [i].curPos = pos; 
 				m_points [i].oldPos = pos; 
 				m_points [i].moveable = true; 
 			}
-
 			// create constraints
 			for (int i = 0; i < m_numpoints;i++)
 			{
-				int x = i % 10; // 10 is the width
-				int y; 
-				if (i < m_numpoints - 1)
-					y = (i / 10) + 1; // Fix this so it wraps around instead. 
-				else
-					y = (i / 10); 
+				//int x = i % 10; // 10 is the width
+				//int y; 
+				//if (i < m_numpoints - 1)
+				//	y = (int)(i / 10); // Fix this so it wraps around instead. 
+				//else
+				//	y = (int)(i / 10); 
 				
-				int i1 = x + 10 * y; 
+				int i1 = i + 1; // x + 10 * y; 
 
-				Debug.Log ("i1 is: " + i1 + " at: " + i); 
+				//Debug.Log ("i1 is: " + i1 + " at: " + i); 
+
 				float len;
 				if (i < m_numpoints - 1)
 					len = (m_points[i].curPos - m_points[i1].curPos).magnitude;
@@ -181,6 +270,7 @@ namespace Alex
 				m_constraints[i].index1 = i1;
 				//c++;	
 			}
+*/
 
 /*			int c = 0;
 
